5745ca6f799e1588b64567f21f80e4e6596b48fd
Ashvin <68969607+betheashvin@users.noreply.github.com>
2025-12-19 18:58:39 +0500

    Refactor Hero component to use Link instead of anchor tag; add vercel.json for routing configuration

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,22 @@
-import "./HeroStyles.css"
+import "./HeroStyles.css";
+import { Link } from "react-router-dom";
 
 function Hero(props) {
-    return (
-        <>
-        <div className={props.cName}>
-            <img alt="Heroimg" src={props.heroImg}/>
+  return (
+    <>
+      <div className={props.cName}>
+        <img alt="Heroimg" src={props.heroImg} />
 
         <div className="hero-text">
-            <h1>{props.title}</h1>
-            <p>{props.text}</p>
-            <a href={props.url} className={props.btnClass}>
-                {props.buttonText}
-            </a>       
-            </div>    
+          <h1>{props.title}</h1>
+          <p>{props.text}</p>
+          <Link to={props.url} className={props.btnClass}>
+            {props.buttonText}
+          </Link>
         </div>
-        </>
-    );
+      </div>
+    </>
+  );
 }
 
 export default Hero;
